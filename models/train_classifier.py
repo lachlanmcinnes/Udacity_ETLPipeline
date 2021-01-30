6325ec90a3822d417f721922f775b44f309f089c
lachlan mcinnes <mcinnes_01@hotmail.com>
2021-01-30 05:28:20 +0000

    UPDATE: add grid search for pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -84,7 +84,6 @@ def build_model():
     parameters = {
         'vect__ngram_range': ((1, 1), (1, 2)),
         'vect__max_df': (0.5, 0.75, 1.0),
-        'vect__max_features': (None, 5000, 10000),
         'tfidf__use_idf': (True, False)
     }
     
